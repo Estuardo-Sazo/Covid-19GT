9f788baa6f44bbf158e19e133d2da97d5cdab2b2
Jaime Sazo <estuardosazo1206@gmail.com>
2020-05-18 11:35:16 -0600

    Create Parametros.php

---
+++ include/php/Parametros.php
@@ -0,0 +1,19 @@
+<?php
+/*define("SERVER","localhost");
+define("USER","root");
+define("PASSWORD","");
+define("DB","db_covid19");
+define("CHAR","utf-8");*/
+
+define("SERVER","localhost");
+define("USER","id11615868_tecnosazo");
+define("PASSWORD","Tecnosazo");
+define("DB","id11615868_db_principal");
+define("CHAR","utf-8");
+
+new Pass @Sazo12061998
+
+
+
+
+?>
